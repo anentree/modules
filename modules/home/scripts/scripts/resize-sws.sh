29596ea346b6114b210d2b25d2899b93a8fb5ca7
An Entree <58378327+anentree@users.noreply.github.com>
2025-03-29 16:41:29 -0500

    added joplin, removed obsidian
    so far, doesn't work

--- modules/home/scripts/scripts/resize-sws.sh
+++ modules/home/scripts/scripts/resize-sws.sh
@@ -4,7 +4,7 @@ resize-sws() {
     # Wait for the window to appear or disappear (adjust the sleep time if needed)
     sleep 0.1
     
-    if hyprctl activewindow | grep -q -E "class: (Spotify|teams-for-linux|kitty|obsidian|discord)"; then
+    if hyprctl activewindow | grep -q -E "class: (Spotify|teams-for-linux|kitty|joplin|discord)"; then
        hyprctl dispatch resizeactive exact 90% 85%
        hyprctl dispatch float
        hyprctl dispatch centerwindow
