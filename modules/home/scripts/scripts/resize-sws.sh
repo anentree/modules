2b90ae2f410d4b6734fd48b8d1a198a96c7289a4
An Entree <58378327+anentree@users.noreply.github.com>
2025-03-31 11:36:25 -0400

    fixed joplin issues

--- modules/home/scripts/scripts/resize-sws.sh
+++ modules/home/scripts/scripts/resize-sws.sh
@@ -4,7 +4,7 @@ resize-sws() {
     # Wait for the window to appear or disappear (adjust the sleep time if needed)
     sleep 0.1
     
-    if hyprctl activewindow | grep -q -E "class: (Spotify|teams-for-linux|kitty|joplin|discord)"; then
+    if hyprctl activewindow | grep -q -E "class: (Spotify|teams-for-linux|kitty|net.cozic.joplin_desktop|discord)"; then
        hyprctl dispatch resizeactive exact 90% 85%
        hyprctl dispatch float
        hyprctl dispatch centerwindow
