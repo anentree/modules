6d678aad0ce4ce34f2fcd0d5d93a2548d68dda9b
An Entree <58378327+anentree@users.noreply.github.com>
2025-04-28 16:36:09 -0500

    optimized boot via fstrim and conditioned power button

--- modules/core/services.nix
+++ modules/core/services.nix
@@ -1,34 +1,47 @@
-{ config, pkgs, ... }: 
+{ config, pkgs, lib, ... }:
+
+let
+  host = config.networking.hostName;
+in
 {
+  systemd.services.fstrim = {
+    enable = false;
+    wantedBy = lib.mkForce [ ];
+  };
+
   services = {
-    avahi = { # for printing and scanning
+    avahi = {
       enable = true;
       nssmdns4 = true;
       openFirewall = true;
     };
     dbus.enable = true;
     fstrim.enable = true;
-    gnome.gnome-keyring.enable = true;
     gvfs.enable = true;
     keyd = {
       enable = true;
       keyboards.default.settings.main = {
-              esc = "capslock";
-              tab = "overload(control, tab)";
-              capslock = "overload(meta, esc)";
-              backslash = "overload(control, backslash)";
-              enter = "overload(meta, enter)";
-              # rightalt = "layer(meta)";
+        esc = "capslock";
+        tab = "overload(control, tab)";
+        capslock = "overload(meta, esc)";
+        backslash = "overload(control, backslash)";
+        enter = "overload(meta, enter)";
+        # rightalt = "layer(meta)";
       };
     };
+    logind.extraConfig = lib.mkMerge [
+      (lib.mkIf (host == "laptop") ''
+        # Don't shutdown when power button is short-pressed
+        HandlePowerKey=ignore
+      '')
+      (lib.mkIf (host != "laptop") ''
+        # Shutdown when power button is short-pressed
+        HandlePowerKey=poweroff
+      '')
+    ];
     printing = {
       enable = true;
       drivers = [ pkgs.hplipWithPlugin ];
     };
   };
-  services.logind.extraConfig = ''
-    # don't shutdown when power button is short-pressed
-    HandlePowerKey=ignore
-  '';
 }
-
