0a4ec380672d31d921ace1113af8d74dcfa92f59
An Entree <58378327+anentree@users.noreply.github.com>
2025-05-17 14:26:17 -0500

    going back to old faithful

--- modules/home/scripts/scripts/resize-sws.sh
+++ modules/home/scripts/scripts/resize-sws.sh
@@ -3,14 +3,14 @@
 resize-sws() {
     # Wait for the window to appear or disappear (adjust the sleep time if needed)
     sleep 0.01
+    
+    if hyprctl activewindow | grep -q -E "class: (Spotify|teams-for-linux|kitty|obsidian|discord|net.cozic.joplin_desktop)"; then
+       hyprctl dispatch float
+       hyprctl dispatch resizeactive exact 90% 85%
+       hyprctl dispatch centerwindow
 
-    active_class=$(hyprctl activewindow | grep -oP 'class=\K[^ ]+')
-    if [ "$active_class" = "Spotify" ] || [ "$active_class" = "teams-for-linux" ] || [ "$active_class" = "kitty" ] || [ "$active_class" = "net.cozic.joplin_desktop" ] || [ "$active_class" = "discord" ]; then
-        hyprctl dispatch resizeactive exact 90% 85%
-        hyprctl dispatch float
-        hyprctl dispatch centerwindow
     fi
-    }
+}
 
 resize-sws
 
